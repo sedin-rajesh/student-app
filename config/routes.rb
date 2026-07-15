4af6b8a6a58cd5d18862f8e0502568fdbdcfb954
sedin-rajesh <rajesh.s@sedintechnologies.com>
2026-07-15 16:58:04 +0530

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   authenticated :user do
     root "dashboard#index", as: :authenticated_root
@@ -11,11 +10,10 @@ Rails.application.routes.draw do
   resources :students do
     member do
       delete :remove_profile_photo
+      delete "documents/:attachment_id", action: :remove_document, as: :remove_document
     end
   end
-  resources :students do
-    delete :remove_document, on: :member
-  end
+
   resources :users, only: [ :index ]
   namespace :api do
     namespace :v1 do
@@ -23,11 +21,7 @@ Rails.application.routes.draw do
         post "login", to: "sessions#create"
         delete "logout", to: "sessions#destroy"
       end
-      resources :students do
-        member do
-          get :report_card
-        end
-      end
+      resources :students
       resources :users do
         collection do
           get :teachers_by_subject
