44effad4f20a03a93a3784ac28a4f583fc57c85c
sedin-rajesh <rajesh.s@sedintechnologies.com>
2026-07-24 12:36:40 +0530

    changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM", ENV.fetch("SMTP_USERNAME", "noreply@example.com"))
+  default from: ENV.fetch("MAIL_FROM")
   layout "mailer"
 end
